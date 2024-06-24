8708fc98d82e5e0f843f8996b06123042956864d
Ales Rechtorik <foxxcz@gmail.com>
2024-06-24 21:21:06 +0200

    update registration link

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,7 @@
 export function Register() {
   return (
     <div class="fixed hidden md:block bottom-20 right-5 z-50 items-center justify-center">
-      <a href="https://app.zerops.io/registeration" target="_blank">
+      <a href="https://zerops.io" target="_blank">
         <div class=" font-bold rounded-md bg-[#f7f7f7] p-3 px-5 py-3 font-mono text-xs hover:bg-[#fbfbfb] text-[#415F90] duration-100 ">
           Register and Get up to $65 free Credit 🎉{" "}
         </div>
